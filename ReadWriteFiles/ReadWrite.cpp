d54397744cdd0f0963dc9f3f7e0553d9dd8e5741
Ben S <schrotbe2021@mountunion.edu>
2020-03-26 15:51:27 -0400

    Finished simple read/write program to .dat file.

--- ReadWriteFiles/ReadWrite.cpp
+++ ReadWriteFiles/ReadWrite.cpp
@@ -1,3 +1,34 @@
 #include <fstream>
 #include <iostream>
 
+using namespace std;
+
+int main()
+{
+	string data;
+
+	// Writing to file
+	ofstream file;
+	file.open("file.dat", ios::out);
+
+	cout << "Writing to file\n";
+	
+	cout << "Enter your name: ";
+	cin >> data;
+	file << data << endl;
+
+	file.close();
+
+	// Reading from file
+	ifstream inFile;
+	inFile.open("file.dat");
+
+	cout << "Reading data\n";
+
+	inFile >> data;
+	cout << data << endl;
+
+	inFile.close();
+
+	return 0;
+}
